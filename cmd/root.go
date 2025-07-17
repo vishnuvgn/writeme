5fac1a5e822915eac46de98b1e646cf89f7b3757
Vishnu Venugopal <vishnuvgn05@gmail.com>
2025-07-17 15:52:57 -0400

    altered version flag in root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -16,7 +17,9 @@ var rootCmd = &cobra.Command{
 	Use:     "writeme",
 	Short:   "An AI README documenter",
 	Long:    `An AI README editor where you can commit messages to and have it format and append to your readme. Can connect to AI endpoints including OpenAI and Llama.`,
-	Version: version, // Use the package var directly
+	Version: fmt.Sprintf("%s\ncommit: %s\nbuilt at: %s", version, commit, date),
+
+	// Version: version, // Use the package var directly
 }
 
 // Execute runs the root command.
